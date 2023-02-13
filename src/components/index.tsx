714e187626506cfd1734ddaccfbca9b62370d70f
tjfaithpro <tjfaithpro@gmail.com>
2023-02-13 23:44:20 +0100

    added signup as a business

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,3 +4,8 @@ export { default as Header } from './common/Header';
 // login coponents
 export { default as LoginComponent } from './login-signup/login';
 export { default as SingupComponent } from './login-signup/signup';
+
+// Business Signup
+export { default as BasicInfo } from './businessSignup/basicInfo';
+export { default as BusinessData } from './businessSignup/BusinessData';
+export { default as BusinessDocuments } from './businessSignup/BusinessDocuments';
