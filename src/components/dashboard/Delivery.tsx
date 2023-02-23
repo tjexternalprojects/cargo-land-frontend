327766cb0231b701f4578d5ef8b017ba939f43e8
Tejiri Faith <tjfaithpro@gmail.com>
2023-02-23 01:14:24 +0100

    update

--- src/components/dashboard/Delivery.tsx
+++ src/components/dashboard/Delivery.tsx
@@ -1,25 +1,40 @@
-import { NewShipmentForm, RecipientDetails } from '..';
+import { NewShipmentForm, RecipientDetails } from "..";
+import { Link } from 'react-scroll';
+
 const Delivery = () => {
-	return (
-		<>
-			<div className=" tracking-widest text-sm text-gray-500 p-10 font-bold">NEW SHIPMENT</div>
-			<div className="px-10  flex gap-3 ">
-				<div>
-					<div className="mt-10 border-l-2 space-y-2">
-						<div className=" border-l-4 border-l-blue-900 pl-4 text-blue-900 font-bold">
-							Package&nbsp;Details
-						</div>
-						<div className="   pl-5">Recipient&nbsp;Details</div>
-						<div className="   pl-5">Payment</div>
-					</div>
-				</div>
-				<div className=" w-full ">
-					<NewShipmentForm />
-					<RecipientDetails />
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className=" tracking-widest text-sm text-gray-500 p-10 font-bold fixed">
+        NEW SHIPMENT
+      </div>
+      <div className="px-10  flex gap-3 ">
+        <div className="fixed mt-16">
+          <div className="mt-10 border-l-2 space-y-2">
+			<Link to="form" smooth={true} duration={500}>
+              <div className=" border-l-4 border-l-blue-900 pl-4 text-blue-900 font-bold cursor-pointer">
+                Package&nbsp;Details
+              </div>
+			  </Link>
+            <div>
+			<Link to="recipient" className="pl-5 cursor-pointer" smooth={true} duration={500}>
+
+                Recipient&nbsp;Details
+            </Link>
+            </div>
+            <div className="pl-5 cursor-pointer">Payment</div>
+          </div>
+        </div>
+        <div className=" w-full pl-16 space-y-10">
+          <div id="form" className="inline-flex w-full pt-20">
+            <NewShipmentForm />
+          </div>
+          <div id="recipient" className="inline-flex w-full">
+            <RecipientDetails />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Delivery;
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,40 +1,38 @@
-import { ReactNode, useState, useContext } from 'react';
-import { Outlet } from 'react-router-dom';
-import { Sidebar, TopBar } from '../components';
-import { GiHamburgerMenu } from '../assets';
-import { useApp, AppContext, AppContextType } from '../context/IndexContext';
+import { ReactNode, useState, useContext } from "react";
+import { Outlet } from "react-router-dom";
+import { Sidebar, TopBar } from "../components";
+import { GiHamburgerMenu } from "../assets";
+import { useApp, AppContext, AppContextType } from "../context/IndexContext";
 
 const Dashboard = () => {
-	const { toggleAdminSideBar } = useApp();
-	const { setState } = useContext<AppContextType>(AppContext);
+  const { toggleAdminSideBar } = useApp();
+  const { setState } = useContext<AppContextType>(AppContext);
+// w-72
+  return (
+<div className="flex bg-gradient-to-tr from-white via-white to-slate-50 min-h-screen">
+  <div className={` w-72 z-10 ${toggleAdminSideBar ? "flex" : "md:flex hidden"}`}>
+    <Sidebar />
+  </div>
+      <div className="bg-white flex items-center justify-center py-3 fixed  w-full pl-72 pr-10 ">
+        <div>
+          <GiHamburgerMenu
+            className={`md:hidden text-4xl`}
+            onClick={() =>
+              setState((prevState) => ({
+                ...prevState,
+                toggleAdminSideBar: true,
+              }))
+            }
+          />
+        </div>
+        <TopBar />
+      </div>
+    <div className="pt-16 ">
+      <Outlet />
+    </div>
+</div>
 
-	return (
-		<div className="flex bg-gradient-to-tr from-white via-white to-slate-50 min-h-screen ">
-			<div className={`w-72  ${toggleAdminSideBar ? 'flex' : 'md:flex hidden'}`}>
-			<Sidebar />
-			</div>
-			<div className=" fixed w-full bg-white flex items-center justify-center py-3 border-2 border-red-800 ">
-				<div className=" h-10 w-full  px-10 ">
-					<div>
-						<GiHamburgerMenu
-							className={`md:hidden text-4xl`}
-							onClick={() =>
-								setState((prevState) => ({
-									...prevState,
-									toggleAdminSideBar: true,
-								}))
-							}
-						/>
-					</div>
-					<TopBar />
-				</div>
-			</div>
-			<div className='pt-16'>
-
-			<Outlet />
-			</div>
-		</div>
-	);
+  );
 };
 
 export default Dashboard;
