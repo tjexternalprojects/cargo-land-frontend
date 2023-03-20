a61fb372212d51fd23bb331084073c7225e8c6e1
Tejiri Faith <tjfaithpro@gmail.com>
2023-03-21 00:53:22 +0100

    UPDATED

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -28,12 +28,8 @@ const Header = () => {
 				<div className="z-20 ">
 					<button
 						onClick={() => setIsOpen(!isOpen)}
-						className={`flex items-center text-sm md:text-md  text-red-400 rounded-3xl px-4  md:px-8 py-2  hover:transition-all duration-150 ease-in-out hover:shadow-xl hover:shadow-blue-100  
-						${
-							!isOpen
-								? '  bg-gradient-to-t from-white via-slate-100 to-slate-100 shadow-lg shadow-slate-300  '
-								: ''
-						}`}
+						className={`flex items-center text-sm md:text-md  text-red-400 rounded-md px-4  md:px-8 py-2  hover:transition-all duration-150 ease-in-out hover:shadow-xl hover:shadow-blue-100  
+					`}
 					>
 						Sign&nbsp;in
 						<BiMenuAltRight className=" md:hidden text-3xl" />
