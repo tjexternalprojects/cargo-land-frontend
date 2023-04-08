ad6954fab04174a1941211b2c57d453de621a190
Tejiri Faith <tjfaithpro@gmail.com>
2023-04-08 15:13:40 +0100

    upadated super admin

---
+++ src/components/superAdminDashboard/Users/userDetails.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const userDetails = () => {
+  return (
+    <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima ipsam consectetur voluptatem commodi fuga vero reprehenderit, quod quisquam id accusamus saepe dolor perferendis beatae ex quasi. Maiores reiciendis minima assumenda.
+      
+    </div>
+  )
+}
+
+export default userDetails
