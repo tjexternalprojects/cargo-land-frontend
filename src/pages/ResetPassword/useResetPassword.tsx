4243665a33157fed4de6f05cd831b2f355339c4e
tjfaithpro <tjfaithpro@gmail.com>
2023-04-12 09:59:34 +0100

    updated

--- src/pages/ResetPassword/useResetPassword.tsx
+++ src/pages/ResetPassword/useResetPassword.tsx
@@ -1,12 +1,13 @@
-import { useState } from "react"
+import { useState } from 'react';
 
-function useResetPassword(){
-    const [resetPassword, setResetPassword] = useState({
-        new_password:'',
-        confirm_password:'',
-        token:''
-    })
-    const handleRestPassword=()=>{}
-    return {handleRestPassword, setResetPassword, resetPassword}
+function useResetPassword() {
+	const [showLoading, setShowLoading] = useState(false);
+	const [resetPassword, setResetPassword] = useState({
+		new_password: '',
+		confirm_password: '',
+		token: '',
+	});
+	const handleRestPassword = () => {};
+	return { handleRestPassword, setResetPassword, showLoading, resetPassword };
 }
-export default useResetPassword
+export default useResetPassword;
